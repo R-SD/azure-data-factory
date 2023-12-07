3285dbcf636998d1f3845841c1990ac435984f4f
R-SD <R-SD>
2023-12-07 05:10:50 +0000

    Transformation Updated 2

--- Tokyo-Olympic-Transformation.py
+++ Tokyo-Olympic-Transformation.py
@@ -23,26 +23,36 @@ extra_configs = configs)
 athletes = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolymic/raw_data/atheletes.csv")
 athletes=athletes.withColumnRenamed("PersonName","Person_Name") #Rename column
 athletes.show()
+athletes.repartition(1).write.mode("overwrite").option("header",'true').csv("/mnt/tokyoolymic/transformed_data/athletes")
 
 # COMMAND ----------
 
 
 coaches = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolymic/raw_data/coaches.csv")
 coaches.show()
+coaches.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed_data/coaches")
 
 # COMMAND ----------
 
+from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType
 entriesgender = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolymic/raw_data/entriesgender.csv")
 entriesgender.show()
+entriesgender=entriesgender.withColumn("Female",col("Female").cast(IntegerType()))\
+    .withColumn("Male",col("Male").cast(IntegerType()))\
+        .withColumn("Total",col("Total").cast(IntegerType()))
+entriesgender.printSchema()                
+entriesgender.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed_data/entriesgender")
 
 # COMMAND ----------
 
 medals = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolymic/raw_data/medals.csv")
 medals=medals.withColumnRenamed("Rank By Total","Rank_By_Total")
 medals.show()
-
+medals.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed_data/medals")
 
 # COMMAND ----------
 
 teams = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolymic/raw_data/teams.csv")
 teams.show()
+teams.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed_data/teams")
